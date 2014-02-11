6b451e4d37229620c167d7924dac52d6c0717ba6
Kevin Giguère <kgiguere@o2web.ca>
2014-02-10 21:00:22 -0500

    test with infinit drag.

--- app/assets/javascripts/tiles.js
+++ app/assets/javascripts/tiles.js
@@ -5,9 +5,10 @@
 
 (function( $ ) {
 	$(function(){
-		var infinitedrag = $.infinitedrag("#wall", {}, {
+		window.infinitedrag = $.infinitedrag("#wall", {}, {
 			width: 100,
 			height: 100,
+			remove_buffer : 10,
 			start_col: 0,
 			start_row: 0
 		});
