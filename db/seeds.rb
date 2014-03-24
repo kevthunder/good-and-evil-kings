2dd85acae31b9c72e789308cd5df08ea93fa1a52
Kevin Giguère <kgiguere@o2web.ca>
2014-03-24 19:53:31 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+MissionStatus.create!([
+  {name: "Going", code: "going"},
+  {name: "Occupying", code: "occupying"},
+  {name: "Returning", code: "returning"}
+])
+MissionType.create!([
+  {name: "Attack", class_name: "AttackMission"},
+  {name: "Trade", class_name: "TradeMission"},
+  {name: "Movement", class_name: "MovementMission"}
+])
+Ressource.create!([
+  {name: "Food"},
+  {name: "Wood"},
+  {name: "Stone"},
+  {name: "Coins"},
+  {name: "Noble coins"}
+])
+SoldierType.create!([
+  {name: "Footman", speed: 5, attack: 10, defence: 3, interception: 5, carry: 10},
+  {name: "Light Cavalry", speed: 10, attack: 5, defence: 3, interception: 10, carry: 20},
+  {name: "Archer", speed: 7, attack: 3, defence: 10, interception: 5, carry: 15}
+])
