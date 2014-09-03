237c40b1796aaea46015e98eae78ab9728926ff0
Kevin Giguère <kgiguere@o2web.ca>
2014-09-02 20:46:00 -0400

    generate random names

--- app/models/name_fragment.rb
+++ app/models/name_fragment.rb
@@ -1,2 +1,17 @@
 class NameFragment < ActiveRecord::Base
+
+  include Randomizable
+  
+  class << self
+    def random_part(group,pos)
+      where(group: group, pos: pos).random
+    end
+    
+    def generate_name(group,format="%{adj} %{noun}")
+      res = format
+      res.gsub(/%\{([^}]+)\}/) do |match| 
+        random_part(group,$1).name
+      end
+    end
+  end
 end
