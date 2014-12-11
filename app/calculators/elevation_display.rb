ec0fc6a320f6f4353efd67da87d80cc68268168b
Kevin Giguère <kgiguere@o2web.ca>
2014-12-10 20:20:38 -0500

    layout

--- app/calculators/elevation_display.rb
+++ app/calculators/elevation_display.rb
@@ -1,7 +1,7 @@
 class ElevationDisplay
   class << self
     def top(x,y,elevation,size,subdivision=1)
-      x * size / 4 / subdivision + y * size / 4 / subdivision + elevation * size / -8 + size / 2;
+      x * size / 4 / subdivision + y * size / 4 / subdivision + elevation * size / -8 + size / 2 + 75;
     end
     def left(x,y,elevation,size,subdivision=1)
       x * size / 2 / subdivision + y * size / 2 / subdivision * -1 + size / 2 * 7
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,22 @@ module ApplicationHelper
     end
     
     def nav_link(link_text, link_path)
-      class_name = current_page?(link_path) ? 'current' : ''
+      class_name = current_page_with_root_alias?(link_path) ? 'current' : ''
 
       content_tag(:li, :class => class_name) do
         link_to link_text, link_path
       end
+    end
+    
+    def model_action_class
+      params[:controller].parameterize + '-' + params[:action]
+    end
+    
+    def current_page_with_root_alias?(url)
+      current_page?(url) ||
+      (
+        current_page?(root_path) &&
+        Rails.application.routes.recognize_path("/") == Rails.application.routes.recognize_path(url)
+      )
     end
 end
